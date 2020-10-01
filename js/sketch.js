bf49227283385302af693b556c5d8fb936cbf778
Nicole <yoyotpe@hotmail.com>
2020-10-01 15:04:53 +0800

    add the ball & its function

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,26 @@
+var posX = Math.floor(Math.random() * 300) + 50;
+var posY = 50;
+var size = 50;
+var changeX = 5;
+var changeY = 5;
+
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	background(220);
+  background(0);
+  fill(240, 138, 93);
+  noStroke();
+  ellipse(posX, posY, size, size);
+  posX += changeX;
+  posY += changeY;
+
+  // change the direction when hitting the wall
+  if (posX < size / 2 || posX > windowWidth - size) {
+		changeX *= -1;
+  }
+  if (posY < size / 2 || posY > windowHeight - size) {
+		changeY *= -1;
+  }
 }
