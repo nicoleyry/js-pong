48b81854c639078b8ad86621ea821554de7ca029
Nicole <yoyotpe@hotmail.com>
2020-10-01 15:08:52 +0800

    add the paddle

--- js/sketch.js
+++ js/sketch.js
@@ -3,6 +3,11 @@ var posY = 50;
 var size = 50;
 var changeX = 5;
 var changeY = 5;
+var paddleX;
+var paddleY;
+var paddleWidth = 100;
+var paddleHeight = 25;
+var started = false;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -10,9 +15,11 @@ function setup() {
 
 function draw() {
   background(0);
+  // draw the ball
   fill(240, 138, 93);
   noStroke();
   ellipse(posX, posY, size, size);
+
   posX += changeX;
   posY += changeY;
 
@@ -23,4 +30,16 @@ function draw() {
   if (posY < size / 2 || posY > windowHeight - size) {
 		changeY *= -1;
   }
+
+  // check if the game is started or not
+  if (!started) {
+		paddleX = windowWidth / 2;
+		paddleY = windowHeight - 100;
+		started = true;
+  }
+
+  // draw the paddle
+  fill(0, 255, 255);
+  noStroke();
+  rect(paddleX, paddleY, paddleWidth, paddleHeight);
 }
